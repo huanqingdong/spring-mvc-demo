0dae7b03490823b24a78502c521a2a468f5f4494
huanqingdong <609139528@qq.com>
2019-10-13 15:03:21 +0800

    format

--- component/src/main/java/app/web/TestController.java
+++ component/src/main/java/app/web/TestController.java
@@ -3,8 +3,6 @@ package app.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,21 +15,18 @@ import javax.servlet.http.HttpServletRequest;
 public class TestController {
 
     @RequestMapping("/test1")
-    public String index(){
+    public String index() {
         return "test1";
     }
 
     @RequestMapping("/test2")
-    public String test2(){
+    public String test2() {
         return "test2";
     }
 
     @RequestMapping("/test3")
-    public String test3(HttpServletRequest request){
-
-        WebApplicationContext ac = WebApplicationContextUtils.getWebApplicationContext(request.getSession().getServletContext());
-        System.out.println("test3 called");
-        log.info("");
+    public String test3(HttpServletRequest request) {
+        log.info("test3 called");
         return "test3";
     }
 
